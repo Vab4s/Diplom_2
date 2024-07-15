e2db01662f2e1568c28db93cf90ad65d9e55a090
Vadim Yakovlev <alienvab3@yandex.ru>
2024-07-15 20:31:44 +0200

    create order, change user, get

--- tests/test_create_order.py
+++ tests/test_create_order.py
@@ -1,12 +1,16 @@
 import allure
 import requests
-import re
 
 from helpers.register_random_user import register_new_user_and_return_userdata_and_response, generate_random_string
 from helpers.delete_user import delete_user
 from helpers.random_order import order
-from data.endpoints import POST_CREATE_ORDER
+from helpers.response_body_check import response_body_create_order_check
 
+from data.endpoints import POST_CREATE_ORDER
+from data.response_messages import ERROR_EMPTY_INGREDIENTS, ERROR_ELEVEN_SYMBOLS_HASH
+
+
+@allure.story('Проверка создания заказа')
 class TestCreateOrder:
     @classmethod
     @allure.title('Создание пользователя и необходимых переменных')
@@ -14,59 +18,64 @@ class TestCreateOrder:
         cls.user_data, cls.response_text, cls.response_code = register_new_user_and_return_userdata_and_response()
         cls.access_token = cls.response_text['accessToken']
 
+    @allure.title('Создание заказа авторизованного пользователя с ингредиентами')
     def test_create_order_with_authorization_with_ingredients(self):
         new_order = order()
         response = requests.post(POST_CREATE_ORDER, data=new_order, headers={"Authorization": self.access_token})
-        assert (response.json()['success'] is True
-                and response.json()['name']
-                and len(response.json()['order']['ingredients']) == len(new_order['ingredients'])
-                and response.status_code == 200)
 
+        assert response_body_create_order_check(response) is True and response.status_code == 200
+
+    @allure.title('Создание заказа неавторизованного пользователя с ингредиентами')
     def test_create_order_without_authorization_with_ingredients(self):
         new_order = order()
         response = requests.post(POST_CREATE_ORDER, data=new_order)
-        assert (response.json()['success'] is True
-                and response.json()['name']
-                and re.fullmatch(r'\d{4}', str(response.json()['order']['number']))
-                and response.status_code == 200)
 
+        assert response_body_create_order_check(response) is True and response.status_code == 200
+
+    @allure.title('Создание заказа авторизованного пользователя без ингредиентов')
     def test_create_order_with_authorization_without_ingredients(self):
         new_order = order(0)
         response = requests.post(POST_CREATE_ORDER, data=new_order, headers={"Authorization": self.access_token})
-        assert (response.json()['success'] is False
-                and response.json()['message'] == 'Ingredient ids must be provided'
-                and response.status_code == 400)
 
+        assert response_body_create_order_check(response, message=ERROR_EMPTY_INGREDIENTS) is False and response.status_code == 400
+
+    @allure.title('Создание заказа неавторизованного пользователя без ингредиентов')
     def test_create_order_without_authorization_without_ingredients(self):
         new_order = order(0)
         response = requests.post(POST_CREATE_ORDER, data=new_order, headers={"Authorization": self.access_token})
-        assert (response.json()['success'] is False
-                and response.json()['message'] == 'Ingredient ids must be provided'
-                and response.status_code == 400)
 
+        assert response_body_create_order_check(response,
+                                                message=ERROR_EMPTY_INGREDIENTS) is False and response.status_code == 400
+
+    @allure.title('Создание заказа авторизованного пользователя с некорректным хэшем ингредиентов')
     def test_create_order_with_authorization_with_wrong_hash(self):
         new_order = order(3, hash=False)
         response = requests.post(POST_CREATE_ORDER, data=new_order, headers={"Authorization": self.access_token})
-        assert 'Internal Server Error' in response.text and response.status_code == 500
 
+        assert response_body_create_order_check(response) is False and response.status_code == 500
+
+    @allure.title('Создание заказа неавторизованного пользователя с некорректным хэшем ингредиентов')
     def test_create_order_without_authorization_with_wrong_hash(self):
         new_order = order(3, hash=False)
         response = requests.post(POST_CREATE_ORDER, data=new_order)
-        assert 'Internal Server Error' in response.text and response.status_code == 500
 
+        assert response_body_create_order_check(response) is False and response.status_code == 500
+
+    @allure.title('Создание заказа авторизованного пользователя с некорректным хэшем, состоящим из 12 символов')
     def test_create_order_with_authorization_with_wrong_hash_with_eleven_symbols(self):
         new_order = {'ingredients': [f'{generate_random_string(12)}']}
         response = requests.post(POST_CREATE_ORDER, data=new_order, headers={"Authorization": self.access_token})
-        assert (response.json()['success'] is False
-                and response.json()['message'] == 'One or more ids provided are incorrect'
-                and response.status_code == 400)
 
+        assert response_body_create_order_check(response,
+                                                message=ERROR_ELEVEN_SYMBOLS_HASH) is False and response.status_code == 400
+
+    @allure.title('Создание заказа неавторизованного пользователя с некорректным хэшем, состоящим из 12 символов')
     def test_create_order_without_authorization_with_wrong_hash_with_eleven_symbols(self):
         new_order = {'ingredients': [f'{generate_random_string(12)}']}
         response = requests.post(POST_CREATE_ORDER, data=new_order)
-        assert (response.json()['success'] is False
-                and response.json()['message'] == 'One or more ids provided are incorrect'
-                and response.status_code == 400)
+
+        assert response_body_create_order_check(response,
+                                                message=ERROR_ELEVEN_SYMBOLS_HASH) is False and response.status_code == 400
 
     @classmethod
     @allure.title('Удаление созданного пользователя')
--- tests/test_get_orders.py
+++ tests/test_get_orders.py
@@ -3,8 +3,12 @@ import requests
 
 from helpers.register_random_user import register_new_user_and_return_userdata_and_response
 from helpers.delete_user import delete_user
-from data.endpoints import GET_USER_ORDERS
+from helpers.response_body_check import response_body_get_check
 
+from data.endpoints import GET_USER_ORDERS
+from data.response_messages import ERROR_UNAUTHORIZED
+
+@allure.story('Проверка получения списка заказов')
 class TestGetOrders:
     @classmethod
     @allure.title('Создание пользователя и необходимых переменных')
@@ -12,15 +16,18 @@ class TestGetOrders:
         cls.user_data, cls.response_text, cls.response_code = register_new_user_and_return_userdata_and_response()
         cls.email, cls.password, cls.name = cls.user_data
         cls.access_token = cls.response_text['accessToken']
-    def test_get_order_authorized_user(self):
-        response = requests.get(GET_USER_ORDERS, headers={"Authorization": self.access_token})
-        assert response.json()['success'] is True and response.status_code == 200
 
+    @allure.title('Получение списка заказов авторизованного пользователя')
+    def test_get_orders_authorized_user(self):
+        response = requests.get(GET_USER_ORDERS, headers={"Authorization": self.access_token})
+
+        assert response_body_get_check(response) is True and response.status_code == 200
+
+    @allure.title('Получение списка заказов неавторизованного пользователя')
     def test_get_order_unauthorized_user(self):
         response = requests.get(GET_USER_ORDERS)
-        assert (response.json()['success'] is False
-                and response.json()['message'] == 'You should be authorised'
-                and response.status_code == 401)
+
+        assert response_body_get_check(response, message=ERROR_UNAUTHORIZED) is False and response.status_code == 401
 
     @classmethod
     @allure.title('Удаление созданного пользователя')
